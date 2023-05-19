b134fd90e54b21fb2d842b71d54a181c880caaf7
saijal333 <130726041+saijal333@users.noreply.github.com>
2023-05-19 17:29:37 +0530

    Add files via upload

---
+++ SnakeLadderAccio/src/main/java/com/example/snakeladderaccio/Board.java
@@ -0,0 +1,83 @@
+package com.example.snakeladderaccio;
+
+import javafx.util.Pair;
+
+import java.util.ArrayList;
+
+public class Board {
+     ArrayList<Pair<Integer, Integer>> positionCoordinates;
+
+     ArrayList<Integer> snakeLadderPosition;
+
+    public Board() {
+        positionCoordinates = new ArrayList<>();
+        populatePositionCoordinates();
+        populateSnakeLadder();
+    }
+    private void populatePositionCoordinates() {
+        positionCoordinates.add(new Pair<>(0, 0));//dummy value
+        for (int i = 0; i < SnakeLadder.height; i++) {
+            for (int j = 0; j < SnakeLadder.width; j++) {
+                //x coordinates
+                int xCord = 0;
+                if (i%2==0){
+                    xCord = j*SnakeLadder.titleSize + SnakeLadder.titleSize/2;
+                }else {
+                    xCord = SnakeLadder.titleSize * SnakeLadder.height - (j * SnakeLadder.titleSize) - SnakeLadder.titleSize / 2;
+                }
+
+                    //y coordinates
+                    int yCord = SnakeLadder.titleSize * SnakeLadder.height - (i * SnakeLadder.titleSize) - SnakeLadder.titleSize / 2;
+                    positionCoordinates.add(new Pair<>(xCord, yCord));
+
+                }
+            }
+        }
+        private void populateSnakeLadder(){
+        snakeLadderPosition = new ArrayList<>();
+        for (int i=0; i<101; i++){
+            snakeLadderPosition.add(i);
+        }
+            snakeLadderPosition.set(4,25);
+            snakeLadderPosition.set(13,46);
+            snakeLadderPosition.set(27,5);
+            snakeLadderPosition.set(33,49);
+            snakeLadderPosition.set(40,3);
+            snakeLadderPosition.set(42,63);
+            snakeLadderPosition.set(43,18);
+            snakeLadderPosition.set(50,69);
+            snakeLadderPosition.set(54,31);
+            snakeLadderPosition.set(62,81);
+            snakeLadderPosition.set(66,45);
+            snakeLadderPosition.set(76,58);
+            snakeLadderPosition.set(74,92);
+            snakeLadderPosition.set(89,53);
+            snakeLadderPosition.set(99,41);
+
+    }
+
+    public int getNewPosition(int currentPosition){
+        if (currentPosition>0 && currentPosition<=100){
+            return  snakeLadderPosition.get(currentPosition);
+        }
+        return -1;
+    }
+        int getXCoordinate(int position){
+        if (position>=1 && position<=100)
+            return positionCoordinates.get(position).getKey();
+        return -1;
+        }
+
+    int getYCoordinate(int position){
+        if (position>=1 && position<=100)
+            return positionCoordinates.get(position).getValue();
+        return -1;
+    }
+      //public static void main(String[] args){
+      //Board board = new Board();
+      //  for (int i = 0; i<board.positionCoordinates.size(); i++){
+      //      System.out.println(i + " $  x :" + board.positionCoordinates.get(i).getKey()
+      //      + "  y : " + board.positionCoordinates.get(i).getValue());
+      //  }
+      //  }
+    }
---
+++ SnakeLadderAccio/src/main/java/com/example/snakeladderaccio/Dice.java
@@ -0,0 +1,8 @@
+package com.example.snakeladderaccio;
+
+public class Dice {
+    public int getRolledDiceValue() {
+        return (int) (Math.random() * 6 * 1);
+    }
+}
+
